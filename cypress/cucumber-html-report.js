205bbc289bf100d3b5aab84fa9cd1c54c7f0eac5
sariful <sariful.islam@fleetcomplete.com>
2021-03-03 17:28:15 -0500

    My first commit

---
+++ cypress/cucumber-html-report.js
@@ -0,0 +1,34 @@
+var reporter = require('cucumber-html-reporter');
+var options = {
+
+        theme: 'bootstrap',
+
+        jsonFile: 'cypress/cucumber-json',
+
+        output: './reports/cucumber-htmlreport.html',
+
+        reportSuiteAsScenarios: true,
+
+        scenarioTimestamp: true,
+
+        launchReport: true,
+
+        metadata: {
+
+            "App Version":"0.3.2",
+
+            "Test Environment": "STAGING",
+
+            "Browser": "Chrome  54.0.2840.98",
+
+            "Platform": "Windows 10",
+
+            "Parallel": "Scenarios",
+
+            "Executed": "Remote"
+
+        }
+
+    };
+
+    reporter.generate(options);
